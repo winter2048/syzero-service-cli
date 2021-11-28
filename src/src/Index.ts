d7dbd3533afaf53c285b792596e8edbb23abe2d2
522112669@qq.com <522112669@qq.com>
2021-11-29 01:27:42 +0800

    2

--- src/src/Index.ts
+++ src/src/Index.ts
@@ -1,5 +1,15 @@
+#!/usr/bin/env node
 import SyZeroServiceCli from './SyZeroServiceCli';
 
-var syZeroServiceCli = new SyZeroServiceCli();
-syZeroServiceCli.start();
-
+function run(argv: string[]) {
+    if (argv[0] === '-v' || argv[0] === '--version') {
+        console.log('  version is 1.0.5');
+    } else if (argv[0] === '-h' || argv[0] === '--help') {
+        console.log('  usage:\n');
+        console.log('  -v --version [show version]');
+    } else {
+        var syZeroServiceCli = new SyZeroServiceCli();
+        syZeroServiceCli.start();
+    }
+}
+run(process.argv.slice(2));
