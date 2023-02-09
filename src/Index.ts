c1f0a8e877c126427529a5f8a8ea2a11a6d7cd8d
winter2048 <522112669@qq.com>
2023-02-10 00:04:52 +0800

    update

--- src/Index.ts
+++ src/Index.ts
@@ -5,7 +5,7 @@ var syZeroServiceCli = new SyZeroServiceCli();
 
 function run(argv: string[]) {
   if (argv[0] === "-v" || argv[0] === "--version") {
-    console.log("  version is 1.0.5");
+    console.log("  version is 1.0.7");
   } else if (argv[0] === "-h" || argv[0] === "--help") {
     console.log("  usage:\n");
     console.log("  -v --version [show version]");
