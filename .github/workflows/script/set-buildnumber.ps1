d7ebe785a23c9a5edd5447446b308acb3ce53db1
winter2048 <522112669@qq.com>
2023-03-21 22:27:04 +0800

    update

--- .github/workflows/script/set-buildnumber.ps1
+++ .github/workflows/script/set-buildnumber.ps1
@@ -1,10 +1,11 @@
 param(
-    [int]$major,
-    [int]$minor,
-    [int]$patch,
-    [string]$tag,
-    [string]$refName
+    [string]$major,
+    [string]$minor,
+    [string]$patch,
+    [string]$tag
 )
+Write-Host "Env:" 
+dir env:
 
 $version = Get-Content "$PSScriptRoot\..\.version"
 if (!$major) {
@@ -17,43 +18,34 @@ if (!$patch) {
     $patch = $version.Split(".")[2]
 }
 
-if ($patch -eq "*") {
-    $version = Get-Content "./.github/workflows/.version"
-    git remote set-url origin https://$($env:GB_NUGET_TOKEN)@github.com/$($env:GITHUB_REPOSITORY).git
-    $tag = git ls-remote --tags | Select-String -Pattern refs/tags/v$version$
+$remote_refs = git ls-remote --tags
 
-    $remote_refs = git ls-remote
-    $tags = $remote_refs | Where-Object { $_.ToLower().Contains('refs/tags/v1.0.') }
-    $latest_tag = $tags | Sort-Object -Property @{ Expression = { [version]::new($_.Split('/v', [StringSplitOptions]::RemoveEmptyEntries)[1]) } } -Descending | Select-Object -First 1
-    $latest_patch = $latest_tag.Split(".")[-1]
-    $patch = [int]$latest_patch + 1
+if ($patch -eq "*") {
+    $tags = $remote_refs | Where-Object { $_.ToLower().Contains("refs/tags/v$major.$minor.") -and !$_.ToLower().Contains('-')}
+    $latest_tag = $tags | Sort-Object -Property { [int]$_.Split('.')[-1] } -Descending | Select-Object -First 1
+    $patch = "0"
+    if ($latest_tag) {
+        $latest_patch = $latest_tag.Split(".")[-1]
+        $patch = [int]$latest_patch + 1
+    }
+    
 }
+
+$CustomBuildVersion = "$major.$minor.$patch"
 
 if ($tag -eq "dev") {
-    $patch = $latest_patch
+    $tags = $remote_refs | Where-Object { $_.ToLower().Contains("refs/tags/v$CustomBuildVersion") -and $_.ToLower().Contains("-$tag")}
+    $latest_tag = $tags | Sort-Object -Property { [int]$_.Split('.')[-1] } -Descending | Select-Object -First 1
+    $devPatch = "1"
+    if ($latest_tag) {
+        $latest_patch = $latest_tag.Split(".")[-1]
+        $devPatch = [int]$latest_patch + 1
+    }
+    $CustomBuildVersion = "$CustomBuildVersion-$tag.$devPatch"
 }
 
-$ProductMajorVersion = $major
-$ProductMinorVersion = $minor
-$ProductpatchVersion = $patch
-$MyCustomBuildVersion = "$ProductMajorVersion.$ProductMinorVersion.$ProductpatchVersion"
-
-if ($refName) {
-    $tag = $refName.Replace("_","-")
-}
-if ($tag -and $tag -ne "master") {
-    $BaselineYear = 2023
-    $CurrentDate = (Get-Date).ToUniversalTime()
-    $StartOfDay = Get-Date -Date $CurrentDate -Hour 0 -Minute 00 -Second 00
-    $BuildMajorVersion = ($CurrentDate.Year - $BaselineYear) * 12 + $CurrentDate.Month
-    $BuildMajorVersion = $BuildMajorVersion * 31 + $CurrentDate.Day
-    $BuildMinorVersion = [math]::floor(((New-TimeSpan -Start $StartOfDay -End $CurrentDate).TotalSeconds) / 2)
-    $MyCustomBuildVersion = "$MyCustomBuildVersion-$tag.$BuildMajorVersion.$BuildMinorVersion"
-}
-$env:BUILD_BUILDNUMBER = $MyCustomBuildVersion
-[Environment]::SetEnvironmentVariable("BUILD_BUILDNUMBER", $MyCustomBuildVersion, "Machine")
-echo "BUILD_BUILDNUMBER=$MyCustomBuildVersion" >> $env:GITHUB_OUTPUT
-echo "BUILD_BUILDNUMBER=$MyCustomBuildVersion" >> $env:GITHUB_ENV
-Write-Host "Setting the value of current build version :  $MyCustomBuildVersion"
-
-dir env:
+Write-Host "Setting the value of current build version :  $CustomBuildVersion" 
+echo "BUILD_BUILDNUMBER=$CustomBuildVersion" >> $env:GITHUB_OUTPUT
+echo "BUILD_BUILDNUMBER=$CustomBuildVersion" >> $env:GITHUB_ENV
+echo "BUILD_VERSION=v$CustomBuildVersion" >> $env:GITHUB_OUTPUT
+echo "BUILD_VERSION=v$CustomBuildVersion" >> $env:GITHUB_ENV
